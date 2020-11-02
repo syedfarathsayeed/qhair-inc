acd097c67a4e72a09d5da7d4c51e4d20d89092d3
Syed Farath Sayeed <syedfarathsayeed@MacBook-Pro-de-Syed.local>
2020-11-02 17:01:04 +0100

    added https redirection with express

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const enforce = require("express-sslify")
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -12,13 +13,14 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', function(req, res) {
+  app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
