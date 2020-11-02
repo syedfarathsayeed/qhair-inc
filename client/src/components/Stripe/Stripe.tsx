5a872318d65ca2e016cede359bffc494be26b65f
Syed Farath Sayeed <syedfarathsayeed@MacBook-Pro-de-Syed.local>
2020-11-02 16:51:21 +0100

    separating our app in client and added stripe payment backend

--- client/src/components/Stripe/Stripe.tsx
+++ client/src/components/Stripe/Stripe.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import StripeCheckout from "react-stripe-checkout";
+import StripeCheckout, { Token } from "react-stripe-checkout";
 import Logo from "resources/images/qhairLogo.png";
+import axios from "axios";
 import useStyles from "./Stripe.styles";
 
 interface StripeProps {
@@ -12,8 +13,20 @@ const Stripe: React.FC<StripeProps> = (props) => {
     const priceForStripe = props.price * 100;
     const publishableKey = "pk_test_51HcA81C6cZibM74nzC65RsBFr53QEy5dhlOrSRCwg5SgAtDqwrq2InBlzp8buQO3txhZHyTsl1shtlblmgIJ7wh400tekHl9UL"
 
-    const onToken = () => {
-        alert("Payment successful")
+    const onToken = (token: Token) => {
+        axios({
+            url: "payment",
+            method: "post",
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(res => {
+            alert("payment successful")
+        }).catch(error => {
+            console.log("payment error", JSON.parse(error))
+            alert("Problem with payment. Please use the given test credit card for payments")
+        })
     }
 
     return (
