5f18500b455038b7ad0aa288fbb2a2ccd7f0cac5
Syed Farath Sayeed <syedfarathsayeed@MacBook-Pro-de-Syed.local>
2020-11-02 11:33:05 +0100

    added app bar icons to make it responsive

--- src/components/Appbar/Appbar.tsx
+++ src/components/Appbar/Appbar.tsx
@@ -4,6 +4,7 @@ import React from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Link, useHistory } from "react-router-dom"
 import { userActions } from "redux/actions/user"
+import { Storefront as ShopIcon, AccountCircle as UserIcon } from "@material-ui/icons"
 import { selectCartItems } from "redux/reducers/cart"
 import { selectUser } from "redux/reducers/user"
 import { AppState, AppThunkDispatch } from "redux/store"
@@ -12,6 +13,7 @@ import whiteLogo from "resources/images/logo.png"
 import CartDropDown from "../CartDropDown"
 import clsx from "clsx"
 import useStyles from "./Appbar_Styles"
+import { IconButton, Tooltip, useMediaQuery } from "@material-ui/core"
 
 const Header: React.FC<{}> = (props) => {
 
@@ -29,6 +31,8 @@ const Header: React.FC<{}> = (props) => {
 
     const history = useHistory()
 
+    const isSmallScreen = useMediaQuery("(max-width: 800px)")
+
     const handleSignInClick = () => {
         history.push("/sign-in")
     }
@@ -44,7 +48,7 @@ const Header: React.FC<{}> = (props) => {
     }
 
     const checkScroll = () => {
-        if (window.pageYOffset > 100) {
+        if (window.pageYOffset > 80) {
             setShowScroll(true)
         } else {
             setShowScroll(false)
@@ -53,24 +57,48 @@ const Header: React.FC<{}> = (props) => {
 
     window.addEventListener("scroll", checkScroll)
 
+    const getActionButtons = () => {
+        return isSmallScreen
+            ? (<>
+                <Tooltip title="Shop">
+                    <IconButton size="small" className={classes.iconButton} onClick={handleShopClick}>
+                        <ShopIcon color={showScroll ? "secondary" : "action"} />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title={loggedUser ? "Sign out" : "Sign in"}>
+                    <IconButton
+                        size="small"
+                        className={classes.iconButton}
+                        onClick={loggedUser ? handleSignOutClick : handleSignInClick}>
+                        <UserIcon color={showScroll ? "secondary" : "action"} />
+                    </IconButton>
+                </Tooltip>
+            </>)
+            : (<>
+                <Button label={"SHOP"} variant="secondary"
+                    onClick={handleShopClick}
+                    className={classes.button} />
+                {
+                    loggedUser
+                        ? <Button label={"Sign Out"}
+                            variant="primary"
+                            onClick={handleSignOutClick}
+                            className={classes.button} />
+                        : <Button label={"Sign in"}
+                            variant="primary"
+                            onClick={handleSignInClick}
+                            className={classes.button} />
+                }
+            </>)
+    }
+
     return (
         <header className={showScroll ? clsx(classes.base, classes.colored) : clsx(classes.base, classes.root)}>
             <Link to={"/home"} className={classes.logo}>
                 <img src={showScroll ? whiteLogo : blackLogo} height={60} alt="Qhaic Inc" />
             </Link>
             <nav className={classes.actions}>
-                <Button label={"SHOP"} variant="secondary"
-                    onClick={handleShopClick}
-                    className={classes.button} />
-                {loggedUser
-                    ? <Button label={"Sign Out"}
-                        variant="primary"
-                        onClick={handleSignOutClick}
-                        className={classes.button} />
-                    : <Button label={"Sign in"}
-                        variant="primary"
-                        onClick={handleSignInClick}
-                        className={classes.button} />}
+                {getActionButtons()}
                 <CartDropDown cartItems={cartItems} showScroll={showScroll} />
             </nav>
         </header>
